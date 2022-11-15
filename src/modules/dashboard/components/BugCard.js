c0c080ab9ba37a77b7712e3a9db4347892b2f9ab
Alex J Garcia <alexshuu@gmail.com>
2022-11-15 08:40:43 -0500

    Hide BugCards on click

--- src/modules/dashboard/components/BugCard.js
+++ src/modules/dashboard/components/BugCard.js
@@ -1,11 +1,20 @@
+import { useState } from 'react';
 import './BugCard.css';
 
 export default function BugCard({
   species,
   children = null,
 }) {
+  const [isVisible, setIsVisible] = useState(true);
+
+  function handleClick() {
+    setIsVisible(false);
+  }
+
+  if (!isVisible) return null;
+
   return (
-    <div className='BugCard'>
+    <div className='BugCard' onClick={handleClick}>
       {species.name}
       <div>
         {children}
