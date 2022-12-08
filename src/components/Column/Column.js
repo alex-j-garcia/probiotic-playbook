17b6fad05c39510863e32f7808cbbfa998e4b0a0
Alex J Garcia <alexshuu@gmail.com>
2022-12-07 20:47:03 -0500

    Update column styles on drag over

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import BugCard from '../BugCard/BugCard';
 
@@ -10,8 +11,27 @@ export default function Column({
   name,
   list = []
 }) {
+  const [isHovered, setIsHovered] = useState(false);
+
+  function handleDragEnter() {
+    console.log(`${name} is being dragged over`);
+    setIsHovered(true);
+  }
+
+  function handleDragExit(e) {
+    console.log(e);
+    console.log(`${name} is no longer dragged over`)
+    setIsHovered(false);
+  }
+
   return (
-    <div className='Column' onDragOver={() => console.log(`${name} has an element over it`)}>{name}
+    <div
+      className='Column'
+      onDragEnter={handleDragEnter}
+      onDragExit={handleDragExit}
+      style={{ background: isHovered ? 'grey' : ''}}
+    >
+      {name}
       <ul>
         {list.map((item, index) => {
           return <BugCard key={index} item={item} isDraggable={true} />
