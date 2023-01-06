d4a47446f7cc1d170025e118746ddf0738764269
Alex J Garcia <alexshuu@gmail.com>
2023-01-05 23:42:27 -0500

    Assign default browser tooltips conditionally

---
+++ src/common/hooks/useCalcTextWidth.js
@@ -0,0 +1,18 @@
+import { useState, useEffect} from 'react';
+
+export default function useCalcTextWidth(word) {
+  const [clientWidth, setClientWidth] = useState(0);
+
+  useEffect(() => {
+    const div = document.createElement('div');
+    div.textContent = word;
+    div.style.fontSize = '1rem';
+    div.style.display = 'inline-block';
+    document.body.appendChild(div);
+    console.log(div.clientWidth);
+    setClientWidth(Math.ceil(div.clientWidth));
+    document.body.removeChild(div);
+  }, []);
+
+  return clientWidth;
+}
--- src/components/BugCard/BugCard.js
+++ src/components/BugCard/BugCard.js
@@ -1,3 +1,4 @@
+import useCalcTextWidth from '../../common/hooks/useCalcTextWidth';
 import Badge from '../../common/components/Badge';
 import './BugCard.css';
 
@@ -5,9 +6,12 @@ export default function BugCard({
   item,
   children = null,
 }) {
+  const { name, type } = item;
+  const clientWidth = useCalcTextWidth(name);
   return (
-    <div title={item.name} className='BugCard'>{item.name}
-      <Badge type={item.type} />
+    <div className='BugCard'>
+      <span title={clientWidth > 152 ? name : null}>{name}</span>
+      <Badge type={type} />
       <div>{children}</div>
     </div>
   );
