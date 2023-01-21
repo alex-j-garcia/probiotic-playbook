b437d6e0c4bf025297ff855f43dc5f52f9284426
Alex J Garcia <alexshuu@gmail.com>
2023-01-21 16:36:52 -0500

    Remove superfluous comments

--- src/common/components/Modal.js
+++ src/common/components/Modal.js
@@ -18,14 +18,3 @@ const OverlayComponents = {
   bugCardList: BugCardList,
   bugDetails: BugDetails,
 };
-
-// The Overlay displays a modal, but the modal is what dictates what the content will be
-
-// The Overlay has to render a certain type of modal:
-  // either the bug modal, or the bug details modal.
-  // first, I should create a common modal component. [√]
-  // The bugCard should have a handler that sets the content of the modal to the bug details.
-  // Anytime a bugCard is clicked on, it should display the bug details modal
-    // that means it has to trigger the overlay
-    // the modal has to render the bug details component
-    // the bug details component's body has to have the expected content
