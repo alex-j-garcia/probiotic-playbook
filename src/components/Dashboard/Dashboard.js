41b451b9f97256591e2dbb1cebaaf8e30a02ac45
Alex J Garcia <alexshuu@gmail.com>
2023-02-09 07:22:46 -0500

    Remove commented code

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -16,17 +16,3 @@ export default function Dashboard() {
     </DropTarget>
   );
 }
-
-
-// import BugTriage from '../BugTriage/BugTriage';
-// import BugBoard from '../BugBoard/BugBoard';
-// import './Dashboard.css';
-
-// export default function Dashboard() {
-//   return (
-//     <main className='Dashboard'>
-//       <BugTriage />
-//       <BugBoard />
-//     </main>
-//   );
-// }
